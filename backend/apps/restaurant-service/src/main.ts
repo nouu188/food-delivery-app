9e752563fb69fd98e886ef4c20982038a68f68ba
Nouu188 <dg.pthinh@gmail.com>
2025-12-15 14:15:02 +0700

    feat: init code base and services, workspaces and libs

--- backend/apps/restaurant-service/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { RestaurantServiceModule } from './restaurant-service.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(RestaurantServiceModule);
-  await app.listen(process.env.port ?? 3000);
-}
-bootstrap();
--- backend/apps/review-service/src/main.ts
+++ backend/apps/review-service/src/main.ts
@@ -1,8 +1,21 @@
+/**
+ * This is not a production server yet!
+ * This is only a minimal backend to get started.
+ */
+
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { ReviewServiceModule } from './review-service.module';
+import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(ReviewServiceModule);
-  await app.listen(process.env.port ?? 3000);
+  const app = await NestFactory.create(AppModule);
+  const globalPrefix = 'api';
+  app.setGlobalPrefix(globalPrefix);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  Logger.log(
+    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+  );
 }
+
 bootstrap();
